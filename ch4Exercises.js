225ea13db9f396e0ca999c5fa03189bca98874d8
PJ Jones <pj.jones4000.com>
2022-07-18 15:34:58 -0500

    woring on dominant writing direction

--- ch4Exercises.js
+++ ch4Exercises.js
@@ -47,6 +47,8 @@
 // };
 // console.log(reverseArrayInPlace([1, 2, 3, 4, 5, 6, 7, 8]));
 
+// all the examples below are from the website. clearly my brain doesn't understand the list concept very well at this point.
+
 const arrayToList = (array) => {
   let list = null;
   for (let i = array.length - 1; i >= 0; i--) {
@@ -55,3 +57,40 @@ const arrayToList = (array) => {
   return list;
 }
 console.log(arrayToList([1, 2, 3]));
+
+function listToArray(list) {
+  let array = [];
+  for (let node = list; node; node = node.rest) {
+    array.push(node.value);
+  }
+  return array;
+}
+
+function prepend(value, list) {
+  return {value, rest: list};
+}
+
+function nth(list, n) {
+  if (!list) return undefined;
+  else if (n == 0) return list.value;
+  else return nth(list.rest, n - 1);
+}
+
+function deepEqual(a, b) {
+  if (a === b) return true;
+  
+  if (a == null || typeof a != "object" ||
+      b == null || typeof b != "object") return false;
+
+  let keysA = Object.keys(a), keysB = Object.keys(b);
+
+  if (keysA.length != keysB.length) return false;
+
+  for (let key of keysA) {
+    if (!keysB.includes(key) || !deepEqual(a[key], b[key])) return false;
+  }
+
+  return true;
+}
+
+
---
+++ ch5Exercises.js
@@ -0,0 +1,23 @@
+let arrays = [[1, 2, 3], [4, 5], [6]];
+console.log(arrays.reduce((a, b) => a.concat(b))); 
+
+function loop(start, test, update, body) {
+  for (let value = start; test(value); value = update(value)) {
+    body(value);
+  }
+}
+
+loop(3, n => n > 0, n => n - 1, console.log);
+
+function every(array, predicate) {
+  for (let element of array) {
+    if (!predicate(element)) return false;
+  }
+  return true;
+}
+
+function every2(array, predicate) {
+  return !array.some(element => !predicate(element));
+}
+
+
